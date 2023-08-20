5f23f750b9be545773f10ae34b386eca9d080e2f
Lorena <lorenazotaj10@gmail.com>
2023-08-20 21:15:23 +0200

    Extra changes

--- src/code/script.js
+++ src/code/script.js
@@ -25,3 +25,51 @@ function createStars() {
   }
 }
 
+
+const accordionContent = document.querySelectorAll('.accordion-content')
+accordionContent.forEach((item, index) => {
+  handleAccordion(item, index)
+})
+
+function handleAccordion(item, index) {
+  let header = item.querySelector('.header')
+  header.addEventListener('click', () => {
+    item.classList.toggle('open')
+
+    let description = item.querySelector('.description')
+    if (item.classList.contains('open')) {
+      header.classList.remove('open')
+
+      description.style.height = `${description.scrollHeight}px`
+      item.querySelector('i').classList.replace('fa-plus', 'fa-minus')
+    } else {
+      header.classList.add('open')
+
+      description.style.height = '0px'
+      item.querySelector('i').classList.replace('fa-minus', 'fa-plus')
+    }
+
+    removeOpen(index)
+  })
+}
+
+function removeOpen(inputIndex) {
+  accordionContent.forEach((item, index) => {
+    if (inputIndex != index) {
+      item.classList.remove('open')
+
+      let description = item.querySelector('.description')
+      description.style.height = '0px'
+      item.querySelector('i').classList.replace('fa-minus', 'fa-plus')
+    }
+  })
+}
+
+
+const detailsButtons = document.querySelectorAll('.details-button')
+detailsButtons.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    let detailsSection = item.querySelector('.extra-details')
+    detailsSection.classList.toggle('hide')
+  })
+})
